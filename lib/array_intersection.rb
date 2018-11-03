2e63622d383d847425974a127d58ebfc8acbb995
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-11-03 15:21:52 -0700

    passing tests

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,23 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+
+
+  expected_intersection = []
+  if array1 == nil || array2 == nil
+    return expected_intersection
+  end
+
+  array1.each do |ele|
+    array2.each do |ele2|
+      if ele == ele2
+        expected_intersection << ele
+      end
+    end
+  end
+
+  return expected_intersection
 end
+
+
+# time complexity is O n2 - nested loops
+# space complexity is On where n is the length of the longest array
